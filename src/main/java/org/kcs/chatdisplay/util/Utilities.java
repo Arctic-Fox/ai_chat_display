7ea546712b0220a305395282ad54d0c4d2fb0041
unixk <unixk@m5-multitronic-unit>
2024-10-12 02:46:23 -0400

    Can now select entire folders and gather all images from all .gz files.

--- src/main/java/org/kcs/chatdisplay/util/Utilities.java
+++ src/main/java/org/kcs/chatdisplay/util/Utilities.java
@@ -1,10 +1,7 @@
 package org.kcs.chatdisplay.util;
 
 import java.awt.image.BufferedImage;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class Utilities {
 	
@@ -28,45 +25,4 @@ public class Utilities {
 		return imageIndex;
 	}
 
-	static class ImageHash {
-		final int hashCode;
-		final BufferedImage image;
-
-		ImageHash(BufferedImage image) {
-			hashCode = image.getData().hashCode();
-			this.image = null;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (obj == null || !(obj instanceof ImageHash)) {
-				return false;
-			}
-			ImageHash other = (ImageHash) obj;
-			return hashCode == other.hashCode && image.getData().equals(other.image.getData());
-		}
-	}
-
-	public static boolean isInSet(BufferedImage checkImage, Set<ImageHash> imageHashes) {
-		for (ImageHash hash : imageHashes) {
-			if (hash.equals(new ImageHash(checkImage))) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-
-
-	public static boolean isInList(BufferedImage checkImage, List<BufferedImage> images) {
-		int hash1 = checkImage.getData().hashCode();
-		for (BufferedImage image : images) {
-			int hash2 = image.getData().hashCode();
-			if (hash1 == hash2) {
-				// images are identical
-				return true;
-			}
-		}
-		return false;
-	}
 }
